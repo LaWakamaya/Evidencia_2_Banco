a8a1683304eda43d954d76b28205b5336c9ded00
Alexandra 0w0 <102385351+Alexandra0w0@users.noreply.github.com>
2022-04-26 14:29:02 -0500

    a

--- EvidenciaBanco/test/Modelo/conexion.java
+++ EvidenciaBanco/test/Modelo/conexion.java
@@ -4,10 +4,31 @@
  */
 package Modelo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author tania
  */
 public class conexion {
+    Connection cn;
+    String url="jdb:mysql://localhost:3306/banco?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    String drv ="com.mysql.jdbc.Driver";
+    String usr ="root";
+    String pwd ="1234";
     
+    public Connection getConnetion(){
+        try{
+            Class.forName(drv);
+            cn=DriverManager.getConnection(url,usr,pwd);
+            JOptionPane.showConfirmDialog(null,"Conexion owo!!");           
+        } catch(Exception e){
+            JOptionPane.showConfirmDialog(null,"No funciono qui qui!!"+ e);           
+        }
+        return cn;
+    }
+    
+            
 }
